12bb4896e3fe32f1a8eb7f8b84b87a3263592ae5
icanurpramanti <icanurpramanti2@gmail.com>
2023-02-27 08:58:09 +0700

    PPDB-Coba

--- resources/views/hero.blade.php
+++ resources/views/hero.blade.php
@@ -1,5 +1,5 @@
 @
-<!-- ======= Hero Section 1 ======= -->
+<!-- ======= Hero Section 2 ======= -->
 <section id="hero">
     <div class="hero-container">
       <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
